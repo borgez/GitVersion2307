06e5fad83f8648d62f094ff957a0c9cb02637d7f
borgez <allionor@gmail.com>
2020-06-04 08:55:31 +0300

    init

---
+++ GitVersion2307/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+
+namespace GitVersion2307
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine($"Hello {typeof(Program).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion}"!);
+		}
+	}
+}
